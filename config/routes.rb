fe283af3e756cfa4871f40000454f6f99e26733a
payne-chris-r <06cpayne@gmail.com>
2016-09-22 10:52:53 -0400

    Complete get /patients feature
    Allow get requests to /patients. Returns all patients via the index
    method in the Patients Controller. Add migrations to set up table
    in db as well.
    
    About to add show

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  resources :patients, only: [:index]
+  # these are the same thing ^
+  # get '/patients', to: 'patients#index'
 end
