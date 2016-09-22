ec0ccf50ec5f8c1e9941abf48fc683b07d0ad536
payne-chris-r <06cpayne@gmail.com>
2016-09-22 13:41:36 -0400

    Allow PATCH to /patients/:id
    Build out feature for PATCH to /patients/:id which routes through
    the #update method in the Patients Controller. Also added curl script
    for that patch request in patients.sh

--- scripts/patients.sh
+++ scripts/patients.sh
@@ -11,3 +11,13 @@ curl --include --request POST http://localhost:3000/patients \
       "sickness": "Runs too fast"
     }
   }'
+
+  # update
+  curl --include --request PATCH http://localhost:3000/patients/3 \
+    --header "Content-Type: application/json" \
+    --data '{
+      "patient": {
+        "name": "Will McBride",
+        "sickness": "Runs too fast"
+      }
+    }'
