71a71ce58daa8b8da546ffb1e4a699ad73a8a21a
payne-chris-r <06cpayne@gmail.com>
2016-09-23 16:02:58 -0400

    Add examples for Doctors and Patients
    Add examples using .create in Examples.rb

--- db/examples.rb
+++ db/examples.rb
@@ -6,7 +6,7 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   cities = City.create([{ name: 'Chicago'  }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # %w(antony jeff matt jason).each do |name|
@@ -16,3 +16,18 @@
 #                password: 'abc123',
 #                password_confirmation: nil)
 # end
+
+Doctor.create([
+              {given_name: "Tucker", surname: "The Rockstar", specialty: "Being a boss"},
+              {given_name: "Jen", surname: "Diagnostic Enthusiast", specialty: "Rocking Diagnostics"},
+              {given_name: "Lauren", surname: "Happy go lucky", specialty: "Bringing joy to everyone!"},
+              {given_name: "Herb", surname: "The Herbster!", specialty: "The Herbinator!"},
+])
+
+Patient.create([
+                { name: 'Jeff Horn', sickness: 'Too cool for school', doctor_id: 2 },
+                { name: 'Chris Payne', sickness: 'Way to excited', doctor_id: 2 },
+                { name: 'Brian Berzellini', sickness: 'Sad' },
+                { name: 'Antony Donovan', sickness: 'Robot Syndrome', doctor_id: 1 },
+                { name: 'Jason Weeks', sickness: 'Oddly large chest', doctor_id: 4}
+              ])
